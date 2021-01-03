b0e311ec5c98d11136f3dce8c7b99b818b25318f
Joonhyun Jeong <terriyou@naver.com>
2021-01-04 00:50:44 +0900

    Create 001_1003_피보나치함수.py

---
+++ python/006_알고리즘공부시작1차/003.실버하위문제/001_1003_피보나치함수.py
@@ -0,0 +1,18 @@
+n = int(input())
+zero_cnt = [1,0,1]
+one_cnt = [0,1,1]
+
+
+for i in range(n):
+    zero_cnt = [1,0,1]
+    one_cnt = [0,1,1]
+    inpt = int(input())
+    
+    if inpt < 3:
+        print(zero_cnt[inpt], one_cnt[inpt])
+        continue
+    for j in range(3, inpt+1):
+        zero_cnt.append(one_cnt[j-1])
+        one_cnt.append(zero_cnt[j-1] + one_cnt[j-1])
+
+    print(zero_cnt[inpt], one_cnt[inpt])
